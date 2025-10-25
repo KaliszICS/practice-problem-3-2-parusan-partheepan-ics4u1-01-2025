69fd9024cbf3649ea5caa44ac970fcdf305a785a
Parusan <Parusan.partheepan@gmail.com>
2025-10-25 10:36:10 -0400

    make changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,27 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+    public static void main(String args[]) {
+        // Test the function
+        System.out.println(towerOfHanoi(3));  // 7
+        System.out.println(towerOfHanoi(4));  // 15  
+        System.out.println(towerOfHanoi(5));  // 31
+        System.out.println(towerOfHanoi(2));  // -1
+        System.out.println(towerOfHanoi(1));  // -1
+        System.out.println(towerOfHanoi(0));  // -1
+    }
+    
+    public static int towerOfHanoi(int num) {
+        // Return -1 if input is less than 3 (as specified)
+        if (num < 3) {
+            return -1;
+        }
+        
+        // Base case: 3 rings is the minimum (7 moves)
+        if (num == 3) {
+            return 7;
+        }
+        
+        // Recursive case: moves(n) = 2 × moves(n-1) + 1
+        return 2 * towerOfHanoi(num - 1) + 1;
+    }
 }
